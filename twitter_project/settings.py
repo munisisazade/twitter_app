5e573e9d0d697cb40311ca8c25af456709de0cba
munisisazade <munisisazade@gmail.com>
2019-01-30 11:30:54 +0400

    Update

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -89,16 +89,28 @@ WSGI_APPLICATION = 'twitter_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'twitter_db_name',
-        'USER': 'twitter_db_user',
-        'PASSWORD': '7ZnTdhxQuFBAnYjdPmkLaNQ5NWTcp222gZRBFaD5CNmJBn7a6',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if DEBUG: # Developmentdirse database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'twitter_db_name',
+            'USER': 'twitter_db_user',
+            'PASSWORD': '7ZnTdhxQuFBAnYjdPmkLaNQ5NWTcp222gZRBFaD5CNmJBn7a6',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get("POSTGRES_DB"),
+            'USER': os.environ.get("POSTGRES_USER"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+            'HOST': os.environ.get("POSTGRES_HOST"),
+            'PORT': os.environ.get("POSTGRES_PORT"),
+        }
+    }
 
 THUMBNAIL_ALIASES = {
     '': {
